9af69556946f6c39204a7efa0c02cf2c0811f974
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-04-01 23:22:20 +0200

    24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format24 hour format

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -46,7 +46,7 @@ function timeToMinutes(iso: string): number {
 
 function formatTime(iso: string): string {
   const d = new Date(iso);
-  return d.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  return d.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false });
 }
 
 /** Parse "2h 25m", "35m", "0m" into { h, m } */
