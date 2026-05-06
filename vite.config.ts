746146698816576659cd349e90860fe8147ab656
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-05-06 08:26:37 +0200

    sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({ telemetry: false })],
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       '/auth': 'http://localhost:8000',
